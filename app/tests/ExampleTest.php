7426e254275b2cbd319870cd8b862225ff2eda75
AdrianiBruss <abrussolo@gmail.com>
2015-02-05 12:12:58 +0100

    update exampleTest

--- app/tests/ExampleTest.php
+++ app/tests/ExampleTest.php
@@ -2,6 +2,23 @@
 
 class ExampleTest extends TestCase {
 
+
+	public function setUp(){
+
+		parent::setUp();
+		Artisan::call('migrate');
+		Artisan::call('db:seed');
+
+
+	}
+
+	public function tearDown(){
+
+		parent::tearDown();
+		Artisan::call('migrate:reset');
+		
+	}
+
 	/**
 	 * A basic functional test example.
 	 *
