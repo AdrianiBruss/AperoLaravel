cd1a9b8a9c1de721b83cdb3761886743d70c2c4b
AdrianiBruss <abrussolo@gmail.com>
2015-02-05 11:35:49 +0100

    update database testing

--- app/config/testing/database.php
+++ app/config/testing/database.php
@@ -3,9 +3,15 @@ return array(
 
     'default' => 'mysql',
     'connections' => array(
-        'host'      => 'localhost',
-        'database'  => 'db_aperoLaravel',
-        'username'  => 'root',
-        'password'  => 'adiibou94',
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => getenv('DB_HOST'),
+            'database'  => getenv('DB_NAME'),
+            'username'  => getenv('DB_USERNAME'),
+            'password'  => getenv('DB_PASSWORD'),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ),
     )
 );
